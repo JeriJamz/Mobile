6dd897e8ae8ee6c3c6bdd2c720a839cae6c4361c
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-08-21 11:17:28 -0500

    Create ObjC.m

---
+++ ObjC.m
@@ -0,0 +1,15 @@
+//Obj C is sum else
+
+#include<foundation/foundation.h>//Idk the header
+
+int main(int argc, const char * argv[]){//this is how we know its from the C family.
+
+    @autoreleasepool{
+
+	NSLog(@"S.O.S\n",
+	       "I havent compiled this so I dont know if this is a vaild syntax or not",
+               "But I bet im not to far off. Any who was serious about making mobile apps so yea.")
+
+    }
+
+}
